9269d6bacbd0911de091f53b57843e7c75d998b4
adnkh <adnan.khabbaz@gmail.com>
2018-08-01 18:13:49 +0300

    update user id

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ const mongoose = require('mongoose')
 
 const app = express();
 
+const User = require('./models/User');
+
+const user = new User();
+user.id = 21312;
+user.team = 'B043';
+user.food = "TEST";
+user.lastLocation = {
+    lat: 0,
+    lng: 0
+};
+user.save();
 
 mongoose.connect("mongodb://all:abc123@ds263571.mlab.com:63571/hackthon-stage", { useNewUrlParser: true })
